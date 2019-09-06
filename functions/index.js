466e25fc1099700d2ac4152d9d3617dfce084987
Rajiv Khushiram <rajivkhushiram@hotmail.com>
2019-09-06 13:06:50 +1000

    yebo

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,26 @@ const functions = require('firebase-functions');
 const firebase = require('firebase-admin');
 const express = require('express');
 const api = express();
+const api2 = express();
+
 const cors = require('cors');
 
 api.use(
+    cors({
+      origin: "*",
+      allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Content-Length",
+        "X-Requested-With",
+        "Accept"
+      ],
+      methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+      optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    })
+  );
+
+  api2.use(
     cors({
       origin: "*",
       allowedHeaders: [
@@ -25,7 +42,14 @@ api.get("/:id", function(req, res) {
   res.status(200).send(url_id);
 });
 
+api2.get("/", function(req, res) {
+    var url_id = "wtf"
+    res.status(200).send(url_id)
+  });
+
+
 exports.relay = functions.region("asia-northeast1").https.onRequest(api)
+exports.relay2 = functions.region("asia-northeast1").https.onRequest(api2)
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
